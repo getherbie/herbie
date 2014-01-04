df231ec361931ce83e78b605a146c9b28d8ccc01
thomas <thomas@192.168.1.37>
2014-01-04 13:19:10 +0100

    Fixed wrong path

--- demo/web/index.php
+++ demo/web/index.php
@@ -2,23 +2,7 @@
 
 ini_set('display_errors', 1);
 
-require_once(__DIR__ . '/../herbie/vendor/autoload.php');
+require_once(__DIR__ . '/../vendor/autoload.php');
 
 $app = new Herbie\Application('site');
 $app->run();
-
-
-function debug($what)
-{
-    echo "<pre>";
-    print_r($what);
-    echo "</pre>";
-}
-
-/*
-$config = __DIR__ . '/../herbie/config/default.yml';
-$app = Herbie\Application::create($config);
-$app->run();
-
-Herbie\debug($app->getConfig()->nice_urls);
-*/
