3579856740711aa97de6c8250458036bac12b365
Thomas Breuss <>
2014-01-21 07:36:38 +0100

    Added licence information

--- demo/web/index.php
+++ demo/web/index.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Herbie.
+ *
+ * (c) Thomas Breuss <www.tebe.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 ini_set('display_errors', 1);
 
 require_once(__DIR__ . '/../vendor/autoload.php');
