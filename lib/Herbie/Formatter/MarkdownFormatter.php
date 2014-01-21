71b1ea690d02f784c08436a786e85069dcfc8288
Thomas Breuss <>
2014-01-21 07:34:15 +0100

    Added licence information

--- lib/Herbie/Formatter/MarkdownFormatter.php
+++ lib/Herbie/Formatter/MarkdownFormatter.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Herbie.
+ *
+ * (c) Thomas Breuss <www.tebe.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Herbie\Formatter;
 
 use Michelf\MarkdownExtra;
--- lib/Herbie/config.php
+++ lib/Herbie/config.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Herbie.
+ *
+ * (c) Thomas Breuss <www.tebe.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 if (is_null($this) || ('Herbie\Application' != get_class($this))) {
     die('Not allowed to access this file.');
 }
