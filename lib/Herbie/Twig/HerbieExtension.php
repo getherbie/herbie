5feaf290f650d805c550d900dd88e73f93a2832e
Thomas Breuss <thomasbreuss@gmx.ch>
2014-01-24 07:19:11 +0100

    Refactored handling of twig extension, deleted test code.

--- lib/Herbie/Twig/HerbieExtension.php
+++ lib/Herbie/Twig/HerbieExtension.php
@@ -119,168 +119,26 @@ class HerbieExtension extends Twig_Extension
      */
     public function getFunctions()
     {
-
-        $app = $this->app;
-        $ext = $this;
-        $dir = __DIR__;
-
-        include(__DIR__.'/Functions/test3.php');
-
+        $options = ['is_safe' => ['html']];
         return [
-            new Twig_SimpleFunction('test1', include(__DIR__.'/Functions/test1.php'), ['is_safe' => ['html']]),
-
-            include("{$dir}/Functions/Test2.php"),
-
-            new Twig_SimpleFunction('test3', $test3, ['is_safe' => ['html']]),
-
-            new Twig_SimpleFunction('link', function ($route, $label, $attributes = []) {
-                return $this->createLink($route, $label, $attributes);
-            }, ['is_safe' => ['html']]),
-
-            new Twig_SimpleFunction('url', function ($route) {
-                return $this->app['urlGenerator']->generate($route);
-            }, ['is_safe' => ['html']]),
-
-            new Twig_SimpleFunction('absurl', function ($route) {
-                return $this->app['urlGenerator']->generateAbsolute($route);
-            }, ['is_safe' => ['html']]),
-
-            new Twig_SimpleFunction('breadcrumb', function (array $options=[]) use ($app) {
-
-                // Options
-                extract($options);
-                $delim      = isset($delim) ? $delim : '';
-                $homeLink   = isset($homeLink) ? $homeLink : null;
-                $reverse    = isset($reverse) ? (bool)$reverse : false;
-
-                $links = [];
-
-                if(!empty($homeLink)) {
-                    if(is_array($homeLink)) {
-                        $route = reset($homeLink);
-                        $label = isset($homeLink[1]) ? $homeLink[1] : 'Home';
-                    } else {
-                        $route = $homeLink;
-                        $label = 'Home';
-                    }
-                    $links[] = $this->createLink($route, $label);
-                }
-
-                foreach($app['rootPath'] AS $item) {
-                    $links[] = $this->createLink($item->getRoute(), $item->getTitle());
-                }
-
-                if(!empty($reverse)) {
-                    $links = array_reverse($links);
-                }
-
-                $html = '<ul class="breadcrumb">';
-                foreach($links AS $link) {
-                    $html .= '<li>' . $link . '</li>';
-                }
-                $html .= '</ul>';
-
-                return $html;
-
-            }, ['is_safe' => ['html']]),
-
-            new Twig_SimpleFunction('pagetitle', function (array $options=[]) use ($app) {
-
-                extract($options); // delim, siteTite, rootTitle, reverse
-
-                $delim      = isset($delim) ? $delim : ' / ';
-                $siteTitle  = isset($siteTitle) ? $siteTitle : NULL;
-                $rootTitle  = isset($rootTitle) ? $rootTitle : NULL;
-                $reverse    = isset($reverse) ? (bool)$reverse : false;
-
-                $count = count($app['rootPath']);
-
-                $titles = [];
-
-                if(!empty($siteTitle)) {
-                    $titles[] = $siteTitle;
-                }
-
-                foreach($app['rootPath'] AS $item) {
-                    if((1==$count) && $item->isStartPage() && !empty($rootTitle)) {
-                        return $rootTitle;
-                    }
-                    $titles[] = $item->getTitle();
-                }
-
-                if(!empty($reverse)) {
-                    $titles = array_reverse($titles);
-                }
-
-                return implode($delim, $titles);
-
-            }, ['is_safe' => ['html']]),
-
-            new Twig_SimpleFunction('image', function ($src, $width = '', $height = '', $alt = '', $class = "") {
-                return sprintf('<img src="%s" width="%d" height="%d" alt="%s" class="%s">', $src, $width, $height, $alt, $class);
-            }, ['is_safe' => ['html']]),
-
-            new Twig_SimpleFunction('content', function ($segmentId = 0) use ($app) {
-
-                if($this->environment->getLoader() instanceof Twig_Loader_String) {
-                    return $this->renderError('You can not use {{ content() }} in page files.');
-                }
-
-                $page = $app['page'];
-                $segment = $page->getSegment($segmentId);
-
-                if(isset($app['config']['pseudo_html'])) {
-                    $pseudoHtml = $app['config']['pseudo_html'];
-                    $segment = str_replace(
-                        explode('|', $pseudoHtml['from']),
-                        explode('|', $pseudoHtml['to']),
-                        $segment
-                    );
-                }
-
-                $twigged = $app->renderString($segment);
-
-                $formatter = FormatterFactory::create($page->getType());
-                $transformed = $formatter->transform($twigged);
-
-                return sprintf('<div class="placeholder-%s">%s</div>', $segmentId, $transformed);
-
-            }, ['is_safe' => ['html']]),
-
-            new Twig_SimpleFunction('menu', function (array $options=[]) use ($app, $ext) {
-
-                extract($options); // showHidden
-                $showHidden = isset($showHidden) ? (bool)$showHidden : false;
-                $route      = isset($route) ? $route : null;
-
-                $tree = empty($route) ? $app['tree'] : $app['tree']->findByRoute($route);
-
-            	$html = $ext->traversTree($tree, $showHidden);
-                return sprintf('<div class="menu">%s</div>', $html);
-            }, ['is_safe' => ['html']]),
-
-            new Twig_SimpleFunction('sitemap', function (array $options=[]) use ($app, $ext) {
-
-                extract($options); // showHidden
-                $showHidden = isset($showHidden) ? (bool)$showHidden : false;
-
-            	$html = $ext->traversTree($app['tree'], $showHidden);
-
-                return sprintf('<div class="sitemap">%s</div>', $html);
-			}, ['is_safe' => ['html']]),
-
-            new Twig_SimpleFunction('bodyClass', function () use ($app) {
-                $route = trim($app->getRoute(), '/');
-                if(empty($route)) {
-                	$route = 'index';
-                }
-                $layout = $app['page']->getLayout(false);
-                $class = sprintf('page-%s layout-%s', $route, $layout);
-                return str_replace(['/', '.'], '-', $class);
-			}, ['is_safe' => ['html']]),
+            new Twig_SimpleFunction('absurl', array($this, 'functionAbsurl'), $options),
+            new Twig_SimpleFunction('bodyClass', array($this, 'functionBodyClass'), $options),
+            new Twig_SimpleFunction('breadcrumb', array($this, 'functionBreadcrumb'), $options),
+            new Twig_SimpleFunction('content', array($this, 'functionContent'), $options),
+            new Twig_SimpleFunction('image', array($this, 'functionImage'), $options),
+            new Twig_SimpleFunction('link', array($this, 'functionLink'), $options),
+            new Twig_SimpleFunction('menu', array($this, 'functionMenu'), $options),
+            new Twig_SimpleFunction('pagetitle', array($this, 'functionPageTitle'), $options),
+            new Twig_SimpleFunction('sitemap', array($this, 'functionSitemap'), $options),
+            new Twig_SimpleFunction('url', array($this, 'functionUrl'), $options),
         ];
     }
 
+    /**
+     * @param MenuTree $tree
+     * @param bool $showHidden
+     * @return string
+     */
     public function traversTree($tree, $showHidden)
     {
         $html = '<ul>';
@@ -299,6 +157,199 @@ class HerbieExtension extends Twig_Extension
         }
         $html .= '</ul>';
         return $html;
+    }
+
+    /**
+     * @param string $route
+     * @return string
+     */
+    public function functionAbsurl($route) {
+        return $this->app['urlGenerator']->generateAbsolute($route);
+    }
+
+    /**
+     * @return string
+     */
+    public function functionBodyClass()
+    {
+        $route = trim($this->app->getRoute(), '/');
+        if(empty($route)) {
+            $route = 'index';
+        }
+        $layout = $this->app['page']->getLayout(false);
+        $class = sprintf('page-%s layout-%s', $route, $layout);
+        return str_replace(['/', '.'], '-', $class);
+    }
+
+    /**
+     * @param array $options
+     * @return string
+     */
+    public function functionBreadcrumb(array $options=[]){
+
+        // Options
+        extract($options);
+        $delim      = isset($delim) ? $delim : '';
+        $homeLink   = isset($homeLink) ? $homeLink : null;
+        $reverse    = isset($reverse) ? (bool)$reverse : false;
+
+        $links = [];
+
+        if(!empty($homeLink)) {
+            if(is_array($homeLink)) {
+                $route = reset($homeLink);
+                $label = isset($homeLink[1]) ? $homeLink[1] : 'Home';
+            } else {
+                $route = $homeLink;
+                $label = 'Home';
+            }
+            $links[] = $this->createLink($route, $label);
+        }
+
+        foreach($this->app['rootPath'] AS $item) {
+            $links[] = $this->createLink($item->getRoute(), $item->getTitle());
+        }
+
+        if(!empty($reverse)) {
+            $links = array_reverse($links);
+        }
+
+        $html = '<ul class="breadcrumb">';
+        foreach($links AS $link) {
+            $html .= '<li>' . $link . '</li>';
+        }
+        $html .= '</ul>';
+
+        return $html;
+
+    }
+
+    /**
+     * @param string|int $segmentId
+     * @return string
+     */
+    public function functionContent($segmentId = 0) {
+
+        if($this->environment->getLoader() instanceof Twig_Loader_String) {
+            return $this->renderError('You can not use {{ content() }} in page files.');
+        }
+
+        $page = $this->app['page'];
+        $segment = $page->getSegment($segmentId);
+
+        if(isset($this->app['config']['pseudo_html'])) {
+            $pseudoHtml = $this->app['config']['pseudo_html'];
+            $segment = str_replace(
+                explode('|', $pseudoHtml['from']),
+                explode('|', $pseudoHtml['to']),
+                $segment
+            );
+        }
+
+        $twigged = $this->app->renderString($segment);
+
+        $formatter = FormatterFactory::create($page->getType());
+        $transformed = $formatter->transform($twigged);
+
+        return sprintf('<div class="placeholder-%s">%s</div>', $segmentId, $transformed);
+
+    }
+
+    /**
+     * @param string $src
+     * @param int $width
+     * @param int $height
+     * @param string $alt
+     * @param string $class
+     * @return string
+     */
+    public function functionImage($src, $width = '', $height = '', $alt = '', $class = "") {
+        return sprintf('<img src="%s" width="%d" height="%d" alt="%s" class="%s">', $src, $width, $height, $alt, $class);
+    }
+
+    /**
+     * @param string $route
+     * @param string $label
+     * @param array $attributes
+     * @return string
+     */
+    public function functionLink($route, $label, $attributes = []) {
+        return $this->createLink($route, $label, $attributes);
+    }
+
+    /**
+     * @param array $options
+     * @return string
+     */
+    public function functionMenu(array $options=[])
+    {
+        extract($options); // showHidden
+        $showHidden = isset($showHidden) ? (bool)$showHidden : false;
+        $route      = isset($route) ? $route : null;
+
+        $tree = empty($route) ? $this->app['tree'] : $this->app['tree']->findByRoute($route);
+
+        $html = $this->traversTree($tree, $showHidden);
+        return sprintf('<div class="menu">%s</div>', $html);
+    }
+
+    /**
+     * @param array $options
+     * @return string
+     */
+    public function functionPagetitle (array $options=[])
+    {
+
+        extract($options); // delim, siteTite, rootTitle, reverse
+
+        $delim      = isset($delim) ? $delim : ' / ';
+        $siteTitle  = isset($siteTitle) ? $siteTitle : NULL;
+        $rootTitle  = isset($rootTitle) ? $rootTitle : NULL;
+        $reverse    = isset($reverse) ? (bool)$reverse : false;
+
+        $count = count($this->app['rootPath']);
+
+        $titles = [];
+
+        if(!empty($siteTitle)) {
+            $titles[] = $siteTitle;
+        }
+
+        foreach($this->app['rootPath'] AS $item) {
+            if((1==$count) && $item->isStartPage() && !empty($rootTitle)) {
+                return $rootTitle;
+            }
+            $titles[] = $item->getTitle();
+        }
+
+        if(!empty($reverse)) {
+            $titles = array_reverse($titles);
+        }
+
+        return implode($delim, $titles);
+
+    }
+
+    /**
+     * @param array $options
+     * @return string
+     */
+    public function functionSitemap(array $options=[]) {
+
+        extract($options); // showHidden
+        $showHidden = isset($showHidden) ? (bool)$showHidden : false;
+
+        $html = $this->traversTree($this->app['tree'], $showHidden);
+
+        return sprintf('<div class="sitemap">%s</div>', $html);
+    }
+
+    /**
+     * @param string $route
+     * @return string
+     */
+    public function functionUrl($route) {
+        return $this->app['urlGenerator']->generate($route);
     }
 
 }
