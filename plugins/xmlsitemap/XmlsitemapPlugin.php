e484346db33e9ea0a6f15bf680fea408f01caf5f
Thomas Breuss <thomasbreuss@gmx.ch>
2014-12-02 06:11:46 +0100

    [FEATURE] Added XML sitemap plugin

---
+++ plugins/xmlsitemap/XmlsitemapPlugin.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of Herbie.
+ *
+ * (c) Thomas Breuss <www.tebe.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace herbie\plugin\xmlsitemap;
+
+use Herbie;
+use Herbie\Loader\FrontMatterLoader;
+use Herbie\Menu\Page\Item;
+
+class XmlsitemapPlugin extends Herbie\Plugin
+{
+
+    /**
+     * @param Herbie\Event $event
+     */
+    public function onPluginsInitialized(Herbie\Event $event)
+    {
+        $alias = $this->config(
+            'plugins.xmlsitemap.pages.sitemap',
+            '@plugin/xmlsitemap/pages/sitemap.xml'
+        );
+        $path = $this->app['alias']->get($alias);
+        $loader = new FrontMatterLoader();
+        $item = $loader->load($path);
+        $item['path'] = $alias;
+        $event['app']['menu']->addItem(
+            new Item($item)
+        );
+    }
+
+}
