9056510d5199620e332db134687070a34e167e8c
Thomas Breuss <thomasbreuss@gmx.ch>
2014-12-02 06:02:44 +0100

    [FEATURE] Added rss feed plugin

---
+++ plugins/rssfeed/README.md
@@ -0,0 +1,19 @@
+Herbie RSS-Feed Plugin
+======================
+
+`RssFeed` ist ein [Herbie](http://github.com/getherbie/herbie) Plugin, das aus deinen Seiten und Blogposts automatisch
+einen RSS-Feed im XML-Format generiert und als Seite bereit stellt.
+
+
+Installation
+-------------
+
+Das Plugin installierst du via Composer.
+
+	$ composer require getherbie/plugin-rssfeed
+
+Danach aktivierst du das Plugin in der Konfigurationsdatei.
+
+    plugins:
+        rssfeed:
+
---
+++ plugins/rssfeed/RssfeedPlugin.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of Herbie.
+ *
+ * (c) Thomas Breuss <www.tebe.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace herbie\plugin\rssfeed;
+
+use Herbie;
+use Herbie\Loader\FrontMatterLoader;
+use Herbie\Menu\Page\Item;
+
+class RssfeedPlugin extends Herbie\Plugin
+{
+
+    /**
+     * @param Herbie\Event $event
+     */
+    public function onPluginsInitialized(Herbie\Event $event)
+    {
+        $alias = $this->config(
+            'plugins.rssfeed.pages.feed',
+            '@plugin/rssfeed/pages/feed.rss'
+        );
+        $path = $this->app['alias']->get($alias);
+        $loader = new FrontMatterLoader();
+        $item = $loader->load($path);
+        $item['path'] = $alias;
+        $event['app']['menu']->addItem(
+            new Item($item)
+        );
+    }
+
+}
