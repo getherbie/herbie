fb6feb6f797ba43880cc8a1cf6ffb3eb2de695d3
Thomas Breuss <thomasbreuss@gmx.ch>
2014-12-29 09:03:53 +0100

    Send Http header

--- src/ErrorHandler.php
+++ src/ErrorHandler.php
@@ -59,6 +59,7 @@ class ErrorHandler
      */
     public function handleException($exception)
     {
+        $this->sendHttpHeader();
         echo '<pre>'.$this->convertExceptionToString($exception).'</pre>';
         exit(1);
     }
@@ -70,6 +71,7 @@ class ErrorHandler
     {
         $error = error_get_last();
         if($this->isFatalError($error)) {
+            $this->sendHttpHeader();
             $exception = new \ErrorException($error['message'], $error['type'], $error['type'], $error['file'], $error['line']);
             echo '<pre>'.$this->convertExceptionToString($exception).'</pre>';
             exit(1);
@@ -109,6 +111,14 @@ class ErrorHandler
     public function isFatalError($error)
     {
         return isset($error['type']) && in_array($error['type'], [E_ERROR, E_PARSE, E_CORE_ERROR, E_CORE_WARNING, E_COMPILE_ERROR, E_COMPILE_WARNING]);
+    }
+
+    /**
+     * @return void
+     */
+    protected function sendHttpHeader($code = 500)
+    {
+        header("HTTP/1.1 $code");
     }
 
 }
