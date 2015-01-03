0f7564335c7422de66f40db4322dc387819e22c3
Thomas Breuss <thomasbreuss@gmx.ch>
2015-01-03 15:22:43 +0100

    Using regular expressions to parse page

--- src/Loader/PageLoader.php
+++ src/Loader/PageLoader.php
@@ -34,19 +34,21 @@ class PageLoader
     /**
      * @param string $alias
      * @param bool $twigify
+     * @param bool $addDefFields
      * @return array
      * @throws \Exception
      */
-    public function load($alias, $twigify = true)
+    public function load($alias, $twigify = true, $addDefFields = true)
     {
         $content = $this->readFile($alias, $twigify);
         list($yaml, $segments) = $this->parseContent($content);
 
         $data = (array) Yaml::parse($yaml);
-        $data['format'] = pathinfo($alias, PATHINFO_EXTENSION);
-        $data['date'] = $this->extractDateFromPath($alias);
-        $data['path'] = $alias;
-
+        if($addDefFields) {
+            $data['format'] = pathinfo($alias, PATHINFO_EXTENSION);
+            $data['date'] = $this->extractDateFromPath($alias);
+            $data['path'] = $alias;
+        }
         return [
             'data' => $data,
             'segments' => $segments
@@ -63,6 +65,34 @@ class PageLoader
         return $this->parseContent($content);
     }
 
+    public function save($alias, array $data = [], array $segments = [])
+    {
+        // page data
+        $content = '---' . PHP_EOL;
+        $content .= Yaml::dump($data);
+        $content .= '---' . PHP_EOL;
+
+        // page segments
+        if(array_key_exists(0, $segments)) {
+            $content .= $segments[0];
+            $content .= PHP_EOL;
+            unset($segments[0]);
+        }
+        if(array_key_exists('', $segments)) {
+            $content .= $segments[''];
+            $content .= PHP_EOL;
+            unset($segments['']);
+        }
+
+        foreach($segments as $key => $value) {
+            $content .= '--- ' . $key . ' ---' . PHP_EOL;
+            $content .= $value;
+            $content .= PHP_EOL;
+        }
+        $path = $this->alias->get($alias);
+        return file_put_contents($path, $content);
+    }
+
     /**
      * @param string $content
      * @return array
@@ -73,34 +103,38 @@ class PageLoader
         $yaml = '';
         $segments = [];
 
-        $i = 0;
-        $segmentId = 0;
-        foreach(explode("\n", $content) as $line) {
-            // strip \r from end of line
-            $line = rtrim($line, "\r");
-            if (preg_match('/^---$/', $line)) {
+        $matched = preg_match('/^-{3}\r?\n(.*)\r?\n-{3}\R(.*)/ms', $content, $matches);
+
+        if($matched === 1 && count($matches) == 3) {
+
+            $yaml = $matches[1];
+
+            $splitted = preg_split('/^-{3} (.+) -{3}$\n?/m', $matches[2], -1, PREG_SPLIT_DELIM_CAPTURE);
+
+            $count = count($splitted);
+            if($count %2 == 0) {
+                throw new \Exception('Fehler beim Auslesen der Seite.');
+            }
+
+            $segments[] = array_shift($splitted);
+            for($i=0; $i<count($splitted); $i=$i+2) {
+                $key = $splitted[$i];
+                $value = $splitted[$i+1];
+                if(array_key_exists($key, $segments)) {
+                    $segments[$key] .= $value;
+                } else {
+                    $segments[$key] = $value;
+                }
+            }
+
+            $i = 0;
+            $last = count($segments) - 1;
+            foreach($segments as $key => $segment) {
+                $segments[$key] = ($i == $last) ? $segment : preg_replace('/\R?$/', '', $segment, 1);
                 $i++;
-                continue;
-            }
-            if ($i == 1) {
-                // add PHP_EOL to end of line
-                $yaml .= $line . PHP_EOL;
-            }
-            if ($i > 1) {
-                if (preg_match('/^--- ([A-Za-z0-9_]+) ---$/', $line, $matches)) {
-                    $segmentId = $matches[1];
-                    continue;
-                }
-                if (!array_key_exists($segmentId, $segments)) {
-                    $segments[$segmentId] = '';
-                }
-                // add PHP_EOL to end of line
-                $segments[$segmentId] .= $line . PHP_EOL;
             }
         }
-        if ($i < 2) {
-            throw new \Exception("Invalid Front-Matter Block in file {$this->alias}.");
-        }
+
         return [$yaml, $segments];
     }
 
