0cb9cae6c7f72366e987a6381a90049ba0cfda2c
Thomas Breuss <thomasbreuss@gmx.ch>
2015-01-04 14:48:31 +0100

    Using global namespace

--- src/Helper/StringHelper.php
+++ src/Helper/StringHelper.php
@@ -19,7 +19,7 @@ class StringHelper
     public static function unaccent($string)
     {
         if (extension_loaded('intl') === true) {
-            $string = Normalizer::normalize($string, Normalizer::FORM_KD);
+            $string = \Normalizer::normalize($string, \Normalizer::FORM_KD);
         }
         if (strpos($string = htmlentities($string, ENT_QUOTES, 'UTF-8'), '&') !== false) {
             $string = html_entity_decode(preg_replace('~&([a-z]{1,2})(?:acute|caron|cedil|circ|grave|lig|orn|ring|slash|tilde|uml);~i', '$1', $string), ENT_QUOTES, 'UTF-8');
