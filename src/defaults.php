efaed4636efaf283e53a25d6c3f4f45e407a1d48
Thomas Breuss <thomasbreuss@gmx.ch>
2015-01-18 08:42:32 +0100

    Use vars instead app

--- src/defaults.php
+++ src/defaults.php
@@ -9,20 +9,16 @@
  * file that was distributed with this source code.
  */
 
-if (is_null($app) || ('Herbie\Application' != get_class($app))) {
-    die('Not allowed to access this file.');
-}
-
 return [
     'app' => [
-        'path' => $app['appPath']
+        'path' => $APP_PATH
     ],
     'site' => [
-        'path' => $app['sitePath']
+        'path' => $SITE_PATH
     ],
     'media' => [
-        'path' => $app['webPath'] . '/media',
-        'url' => $app['webUrl'] . '/media',
+        'path' => $WEB_PATH . '/media',
+        'url' => $WEB_URL . '/media',
         'images' => 'jpg,gif,png,svg,ico,tiff,bmp,psd,ai',
         'documents' => 'md,pdf,doc,docx,xls,xlsx,ppt,csv,rtf',
         'archives' => 'zip,tar,gz,gzip,tgz',
@@ -31,42 +27,42 @@ return [
         'audio' => 'mp3,m4a,wav,aiff,midi'
     ],
     'layouts' => [
-        'path' => $app['sitePath'] . '/layouts'
+        'path' => $SITE_PATH . '/layouts'
     ],
     'theme' => 'default',
     'pages' => [
-        'path' => $app['sitePath'] . '/pages',
+        'path' => $SITE_PATH . '/pages',
         'extensions' => ['txt', 'md', 'markdown', 'textile', 'htm', 'html', 'rss', 'xml']
     ],
     'posts' => [
-        'path' => $app['sitePath'] . '/posts',
+        'path' => $SITE_PATH . '/posts',
         'extensions' => ['txt', 'md', 'markdown', 'textile', 'htm', 'html', 'rss', 'xml'],
         'blog_route' => 'blog'
     ],
     'data' => [
-        'path' => $app['sitePath'] . '/data',
+        'path' => $SITE_PATH . '/data',
         'extensions' => ['yml', 'yaml']
     ],
     'nice_urls' => false,
     'display_load_time' => false,
     'twig' => [
         'debug' => false,
-        'cache' => false, //$app['sitePath'] . '/cache/twig',
+        'cache' => false, //$SITE_PATH . '/cache/twig',
         'extend' => [
-            'functions' => $app['sitePath'] . '/twig/functions',
-            'filters' => $app['sitePath'] . '/twig/filters',
-            'tests' => $app['sitePath'] . '/twig/tests',
+            'functions' => $SITE_PATH . '/twig/functions',
+            'filters' => $SITE_PATH . '/twig/filters',
+            'tests' => $SITE_PATH . '/twig/tests',
         ]
     ],
     'cache' => [
         'page' => [
             'enable' => false,
-            'dir' => $app['sitePath'] . '/cache/page',
+            'dir' => $SITE_PATH . '/cache/page',
             'expire' => 86400
         ],
         'data' => [
             'enable' => false,
-            'dir' => $app['sitePath'] . '/cache/data',
+            'dir' => $SITE_PATH . '/cache/data',
             'expire' => 86400
         ]
     ],
@@ -74,7 +70,7 @@ return [
     'locale' => 'de_DE.UTF-8',
     'charset' => 'UTF-8',
     'plugins' => [
-        'path' => $app['sitePath'] . '/plugins',
+        'path' => $SITE_PATH . '/plugins',
         'enable' => [],
         'config' => []
     ]
