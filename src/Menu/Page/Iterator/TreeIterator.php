0484a139ab48ba7a50422123f4d95a0fbec35c1b
Thomas Breuss <thomasbreuss@gmx.ch>
2015-07-18 13:32:46 +0200

    Revert "Replaced wrong formatted url in phpdoc"
    This reverts commit 58c6b905410e5b37bd58d5a2545e86adda2c1ed8.

--- src/Menu/Page/Iterator/TreeIterator.php
+++ src/Menu/Page/Iterator/TreeIterator.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of Herbie.
  *
- * (c) Thomas Breuss <http://www.tebe.ch>
+ * (c) Thomas Breuss <www.tebe.ch>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
