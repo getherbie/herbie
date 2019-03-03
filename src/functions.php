6d96eaf7931c1e2522e0867192d50f806b6c455b
tbreuss <thomasbreuss@gmx.ch>
2019-03-03 11:22:08 +0100

    Added filesystem functions
    Signed-off-by: tbreuss <thomasbreuss@gmx.ch>

--- src/functions.php
+++ src/functions.php
@@ -63,3 +63,90 @@ function render_exception(\Throwable $exception): string
 
     return sprintf('<pre>%s</pre>', $message);
 }
+
+/**
+ * Recursively delete a directory and all of it's contents - e.g.the equivalent of `rm -r` on the command-line.
+ * Consistent with `rmdir()` and `unlink()`, an E_WARNING level error will be generated on failure.
+ *
+ * @see http://stackoverflow.com/a/3352564/283851
+ * @param string $dir absolute path to directory to delete
+ * @return bool true on success; false on failure
+ */
+function remove_folder(string $dir): bool
+{
+    if (false === file_exists($dir)) {
+        return false;
+    }
+
+    /** @var \SplFileInfo[] $files */
+    $files = new \RecursiveIteratorIterator(
+        new \RecursiveDirectoryIterator($dir, \RecursiveDirectoryIterator::SKIP_DOTS),
+        \RecursiveIteratorIterator::CHILD_FIRST
+    );
+
+    foreach ($files as $fileinfo) {
+        if ($fileinfo->isDir()) {
+            if (false === rmdir($fileinfo->getRealPath())) {
+                return false;
+            }
+        } else {
+            if (false === unlink($fileinfo->getRealPath())) {
+                return false;
+            }
+        }
+    }
+
+    return rmdir($dir);
+}
+
+/**
+ * @param string $path
+ * @param array|null $skip
+ * @return bool
+ */
+function empty_folder(string $path, ?array $skip = null): bool
+{
+    if (is_null($skip)) {
+        $skip = ['.', '..', '.gitignore'];
+    }
+    /** @var \SplFilInfo[] $files */
+    $files = new \DirectoryIterator($path);
+    $success = true;
+    foreach ($files as $fileInfo) {
+        if (in_array($fileInfo->getFilename(), $skip)) {
+            continue;
+        }
+        $realPath = $fileInfo->getRealPath();
+        if ($fileInfo->isDir()) {
+            $success &= remove_folder($realPath);
+        } else {
+            $success &= unlink($realPath);
+        }
+    }
+    return boolval($success);
+}
+
+/**
+ * @param string $path
+ * @param array|null $skip
+ * @return int
+ */
+function count_files(string $path, ?array $skip = null): int
+{
+    if (is_null($skip)) {
+        $skip = ['.', '..', '.gitignore', '.DS_Store'];
+    }
+    /** @var \SplFileInfo[] $files */
+    $files = new \RecursiveIteratorIterator(
+        new \RecursiveDirectoryIterator($path),
+        \RecursiveIteratorIterator::SELF_FIRST
+    );
+    $count = 0;
+    foreach ($files as $fileInfo) {
+        if (in_array($fileInfo->getFilename(), $skip)) {
+            continue;
+        }
+        $count++;
+    }
+    return $count;
+}
