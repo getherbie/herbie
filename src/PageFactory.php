80195b6f482b143463345b7c4143533a19a412d7
tbreuss <thomasbreuss@gmx.ch>
2022-09-14 06:03:51 +0200

    wip: fix issue with page tree
    Signed-off-by: tbreuss <thomasbreuss@gmx.ch>

--- src/PageFactory.php
+++ src/PageFactory.php
@@ -32,16 +32,41 @@ final class PageFactory
         return new PageList($items);
     }
 
+    private function isIndexPage(string $path): bool
+    {
+        $filename = pathinfo($path, PATHINFO_FILENAME);
+        $filenameWithoutPrefix = preg_replace('/^([0-9])+-/', '', $filename);
+        return $filenameWithoutPrefix === 'index';
+    }
+    
     public function newPageTree(PageList $pageList): PageTree
     {
         $tree = new PageTree();
+
+        // first go through all index pages
         foreach ($pageList as $pageItem) {
-            $route = $pageItem->getParentRoute();
-            $node = $tree->findByRoute($route);
-            if ($node) {
-                $node->addChild(new PageTree($pageItem));
+            if (!$this->isIndexPage($pageItem->path)) {
+                continue;
+            }
+            $parentRoute = $pageItem->getParentRoute();
+            $parent = $tree->findByRoute($parentRoute);
+            if ($parent) {
+                $parent->addChild(new PageTree($pageItem));
             }
         }
+
+        // then go through all non-index pages
+        foreach ($pageList as $pageItem) {
+            if ($this->isIndexPage($pageItem->path)) {
+                continue;
+            }
+            $parentRoute = $pageItem->getParentRoute();
+            $parent = $tree->findByRoute($parentRoute);
+            if ($parent) {
+                $parent->addChild(new PageTree($pageItem));
+            }
+        }
+        
         return $tree;
     }
 
