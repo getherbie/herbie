29bf43d89805ca8823ad4c5837ec6241f6c9985f
tbreuss <thomasbreuss@gmx.ch>
2022-11-24 22:10:14 +0100

    docs: add git activity with latest commits to sidebar
    Signed-off-by: tbreuss <thomasbreuss@gmx.ch>

---
+++ website/site/extend/twig_functions/git_commits.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace website\site\extend\twig_functions;
+
+use Exception;
+
+const CACHE_FILE = __DIR__ . '/git_commits.cache';
+
+function commits_from_cache(): ?string
+{
+    $exists = file_exists(CACHE_FILE);
+    if (!$exists || (time() > strtotime('+2 hours', filemtime(CACHE_FILE)))) {
+        try {
+            $data = implode(file('https://github.com/getherbie/herbie/commits/2.x-develop.atom'));
+        } catch (Exception $e) {
+            return null;
+        }
+        file_put_contents(CACHE_FILE, $data);
+    } else {
+        $data = file_get_contents(CACHE_FILE);
+    }
+    return $data;
+}
+
+return ['git_commits', function () {
+    $commits = commits_from_cache();
+    if ($commits === null) {
+        return '';
+    }
+    $xml = simplexml_load_string($commits);
+    $markup = '<div class="gc">';
+    $markup .= sprintf('<h3 class="gc-header">%s</h3>', 'GitHub Activity');
+    $markup .= sprintf('<div class="gc-subheader">%s</div>', $xml->title);
+    $i = 0;
+    foreach ($xml->entry as $entry) {
+        $markup .= '<div class="gc-entry">';
+        $markup .= sprintf(
+            '<div class="gc-entry-title"><a href="%s">%s</a></div>',
+            $entry->link['href'],
+            $entry->title
+        );
+        $markup .= '<div class="gc-entry-text">';
+        $markup .= sprintf(
+            '<span class="gc-entry-date">%s</span> by <a class="gc-entry-author" href="%s">%s</a>',
+            date('d.m.Y', strtotime((string)$entry->updated)),
+            $entry->author->uri,
+            $entry->author->name
+        );
+        $markup .= '</div>';
+        $markup .= '</div>';
+        if ($i++ >= 9) {
+            break;
+        }
+    }
+    $markup .= '</div>';
+    return $markup;
+}, ['is_safe' => ['all']]];
