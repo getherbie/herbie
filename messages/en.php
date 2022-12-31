f092c83378bf05f0fd675930fec368f46ae9bc38
tbreuss <thomasbreuss@gmx.ch>
2022-12-31 13:13:43 +0100

    fix: revise pagination
    Signed-off-by: tbreuss <thomasbreuss@gmx.ch>

--- messages/en.php
+++ messages/en.php
@@ -1,6 +1,11 @@
 <?php
 
 return [
+    'First page' => 'First page',
     'Herbie CMS' => 'Herbie CMS',
+    'Last page' => 'Last page',
+    'Next page' => 'Next page',
     'Plugin {plugin} enabled but not found.' => 'Plugin "{plugin}" enabled but not found!',
+    'Previous page' => 'Previous page',
+    'There are no entries available.' => 'There are no entries available.',
 ];
